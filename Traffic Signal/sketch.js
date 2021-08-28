259c2b857cb1f0db6a50d89a4e0f832c31f61f1d
unknown <raveen.anand@whitehatjr.com>
2021-08-28 13:54:18 +0530

    First

---
+++ Traffic Signal/sketch.js
@@ -0,0 +1,26 @@
+var yellow,red,green;
+var rectangle;
+function setup() {
+  //size of the canvas to draw on
+  createCanvas(innerWidth, innerHeight);
+
+  // Set the background to white, and move the line to setup();
+  background("white");
+
+  fill("black");
+  rect(100, 50, 100, 300); 
+  rect(140, 350, 20, 300);
+
+}
+
+
+function draw() {
+  
+  red = new Signal(150, 100, "red");
+  red.appear();
+  
+  /***Create objects for Green and Red lights*****/
+  //Add code here: 
+}
+    
+
